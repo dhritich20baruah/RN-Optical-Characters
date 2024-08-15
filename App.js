3586d1b899e70bd2e98ab419f67cf21552d5fa40
Dhriti <dhriti.baruah20@gmail.com>
2024-08-15 07:05:59 +0530

    maps

--- App.js
+++ App.js
@@ -1,11 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import MapView, {Marker} from 'react-native-maps';
+import React, {useState, useEffect} from 'react';
 
 export default function App() {
+  const [mapRegion, setMapRegion] = useState({
+    latitude: 27.4705,
+    longitude: 94.9125,
+    latitudeDelta: 0.0922,
+    longitudeDelta: 0.0421
+  })
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <MapView style={styles.map} region={mapRegion}>
+        <Marker coordinate={mapRegion} title='Pin'/>
+        </MapView>
     </View>
   );
 }
@@ -13,8 +23,9 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+  map: {
+    width: '100%',
+    height: '100%',
   },
 });
