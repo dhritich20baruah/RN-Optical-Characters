6df6e9447d38ce20f3e97ecb3c500ba1ce611686
Dhriti <dhriti.baruah20@gmail.com>
2024-08-18 15:29:30 +0530

    snap and share added

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import { StatusBar } from "expo-status-bar";
 import { Alert, StyleSheet, Text, View, Button } from "react-native";
-import MapView, { Marker } from "react-native-maps";
-import React, { useState, useEffect } from "react";
+import MapView, { Callout, Marker } from "react-native-maps";
+import React, { useState, useEffect, useRef } from "react";
 import * as Location from "expo-location";
 
 let locationOfInterest = [
@@ -27,13 +27,25 @@ export default function App() {
   const [mapRegion, setMapRegion] = useState({
     latitude: 27.4705,
     longitude: 94.9125,
-    latitudeDelta: 0.0922,
-    longitudeDelta: 0.0421,
+    // latitudeDelta: 0.0922,
+    // longitudeDelta: 0.0421,
   });
 
+  const [draggableCoord, setDraggableCoord] = useState({
+    latitude: 27.4705,
+    longitude: 94.9125,
+  });
+  const [count, setCount] = useState(0)
   const onRegionChange = (region) => {
-    console.log(region);
+    // console.log(region);
   };
+
+  const mapRef = useRef()
+
+  const takeSnapShot = async () => {
+    const snapShot = await mapRef.current.takeSnapshot({width: 300, height: 300, result: "base64"})
+    console.log(snapShot)
+  }
 
   const userLocation = async () => {
     let { status } = await Location.requestForegroundPermissionsAsync();
@@ -55,29 +67,17 @@ export default function App() {
     userLocation();
   }, []);
 
-  const showLocationOfInterest = () => {
-    return locationOfInterest.map((item, index) => {
-      return (
-        <Marker
-          key={index}
-          coordinate={item.location}
-          title={item.title}
-          description={item.description}
-        />
-      );
-    });
-  };
-
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
       <MapView
+        ref={mapRef}
         style={styles.map}
         region={mapRegion}
         onRegionChange={onRegionChange}
       >
         <Marker coordinate={mapRegion} title="Pin" />
-        {locationOfInterest.map((item, index) => {
+        {/* {locationOfInterest.map((item, index) => {
           return (
             <Marker
               key={index}
@@ -86,9 +86,23 @@ export default function App() {
               description={item.description}
             />
           );
-        })}
+        })} */}
+        <Marker
+          draggable
+          coordinate={draggableCoord}
+          pinColor="#00ff00"
+          onDragEnd={(e) => setDraggableCoord(e.nativeEvent.coordinate)}
+        />
+        {/* <Marker coordinate={{latitude: 27.4, longitude: 95}} pinColor="#8F00FF" title="increment"/> */}
+        <Marker coordinate={{latitude: 27.45, longitude: 94.9}} pinColor="#8F00FF">
+          <Callout>
+            <Text>Count: {count}</Text>
+            <Button title='increment' onPress={()=>setCount(count + 1)}/>
+          </Callout>
+        </Marker>
       </MapView>
-      <Button onPress={userLocation} title="GET LOCATION" color="red" />
+      <Button title="Snap & Share" onPress={takeSnapShot}/>
+      {/* <Button onPress={userLocation} title="GET LOCATION" color="red" /> */}
     </View>
   );
 }
